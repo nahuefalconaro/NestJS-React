f6f6de85d2122d3dafec06601e53f3535b24f398
nahuefalconaro <nfalconaro5@gmail.com>
2023-08-24 20:43:47 -0300

    add: back

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ],
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "",
+      password: "",
+      database: "", 
+      entities: [
+        "dist/**/**.entity{.ts,.js}" 
+      ],
+      synchronize: true
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
